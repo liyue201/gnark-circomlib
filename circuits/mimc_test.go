a10297a01a04abcbb9a28c3aa8a776bdc2903380
liyue201 <liyue201@126.com>
2022-12-20 16:41:03 +0800

    test mimc

--- circuits/mimc_test.go
+++ circuits/mimc_test.go
@@ -41,3 +41,33 @@ func Test_MiMC(t *testing.T) {
 	internal, secret, public := _r1cs.GetNbVariables()
 	fmt.Printf("public, secret, internal %v, %v, %v\n", public, secret, internal)
 }
+
+type multiMiMC7Test struct {
+	Input [2]frontend.Variable `gnark:",public"`
+	Hash  frontend.Variable
+}
+
+func (t *multiMiMC7Test) Define(api frontend.API) error {
+	hash := MultiMiMC7(api, 91, t.Input[:], 0)
+	api.AssertIsEqual(hash, t.Hash)
+	return nil
+}
+
+func Test_MultiMiMC7(t *testing.T) {
+	assert := test.NewAssert(t)
+	var circuit multiMiMC7Test
+
+	input := []*big.Int{big.NewInt(1), big.NewInt(2)}
+	hash, _ := mimc7.Hash(input, big.NewInt(0))
+
+	as := multiMiMC7Test{
+		Hash: hash,
+	}
+	as.Input[0] = 1
+	as.Input[1] = 2
+
+	t.Logf("hash: %v", hash)
+
+	assert.ProverSucceeded(&circuit, &as, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16), test.WithCompileOpts(frontend.IgnoreUnconstrainedInputs()))
+
+}
